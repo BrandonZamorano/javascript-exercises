64c90081f38e9c9c111dbebbcf54e0c8c0d06d54
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-01-04 16:51:45 -0600

    Finish 02-repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (str, timesToRepeat) {
+  if (timesToRepeat < 0) return "ERROR";
 
+  let repeatedString = "";
+
+  for (let i = 0; i < timesToRepeat; i++) {
+    repeatedString += str;
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
